48cbc9dc442d59c1dfed0c500b0b86df45c74304
EuGuiXtd <guilhermedpjf@gmail.com>
2023-03-10 17:48:19 -0300

    feat: pagina de login sem funcionalidades

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React/* , { useContext, useState } */ from 'react';
+import Header from '../components/Header';
+
+function Login() {
+  return (
+    <Header />
+  );
+}
+
+export default Login;
